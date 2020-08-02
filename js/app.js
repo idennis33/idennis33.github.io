148496bcd1d2c2856070d5e78bc49777ab2d3448
Isaac Dennis <idennisgsu3@gmail.com>
2020-08-01 20:28:38 -0400

    Got timer to show up in the html but still need to add it to the screen.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,37 @@
 // console.log("hello");
 // console.log($);
 
-const timer = 30; 
-const elem = document.getElementById('timer'); 
-const timerId = setInterval(countdown,1000); 
+let timer = 30;
+const elem = document.getElementById("timer");
 
-const countdown = () => {
-    if (timeLeft === 0 ){
-        clearTimeout(timerId)
-        //doSomething(); 
-    }else{
-        elem.innerHTML = timeleft + ' seconds remaining'; 
-        timeLeft --; 
-    }
-}
+const countDown = () => {
+  if (timer ==-1) {
+    clearTimeout(timerId);
+    
+  } else {
+    elem.innerHTML = timer + " seconds remaining";
+    timer--;
+  }
+};
 
+let timerId = setInterval(countDown, 1000);
 
 $(() => {
-    //Grabbing Elements
-    const $openBtn = $('#openModal');
-    const $modal = $('#modal');
-    const $closeBtn = $('#close');
-    //Event Handlers
-    const openModal = () => {
-      $modal.css('display', 'block');
-    }
-    const closeModal = () => {
-      $modal.css('display', 'none');
-    }
-    //Event Listeners
-    $openBtn.on('click', openModal);
-    $closeBtn.on('click', closeModal);
-    setTimeout(openModal, 3000);
-    }); 
+  //Grabbing Elements
+  const $openBtn = $("#openModal");
+  const $modal = $("#modal");
+  const $closeBtn = $("#close");
+  //Event Handlers
+  const openModal = () => {
+    $modal.css("display", "block");
+  };
+  const closeModal = () => {
+    $modal.css("display", "none");
+  };
+  //Event Listeners
+  $openBtn.on("click", openModal);
+  $closeBtn.on("click", closeModal);
+  setTimeout(openModal, 3000);
+});
+
 
