2a99363b767dfb1fb622034a35808b40bb47c64b
Isaac Dennis <idennisgsu3@gmail.com>
2020-08-01 23:47:06 -0400

    Made three functions for now that prompts for userinput, need to get userinput to work promptly.

--- js/app.js
+++ js/app.js
@@ -5,9 +5,8 @@ let timer = 30;
 const elem = document.getElementById("timer");
 
 const countDown = () => {
-  if (timer ==-1) {
+  if (timer == -1) {
     clearTimeout(timerId);
-    
   } else {
     elem.innerHTML = timer + " seconds remaining";
     timer--;
@@ -31,7 +30,38 @@ $(() => {
   //Event Listeners
   $openBtn.on("click", openModal);
   $closeBtn.on("click", closeModal);
-  setTimeout(openModal, 3000);
 });
+let userChoice = "";
+const startTheGame = () => {
+  let userChoice = window.prompt("Are you ready to be questioned?");
+  if (userChoice = "yes" || "yes".toUpperCase()) {
+    alert("Let see how much you know");
+    question1();
+  } else if (userChoice = "no" || "no".toUpperCase()) {
+    alert("That's okay you need more time to study.");
+  }
+};
 
+const question1 = () => {
+  window.prompt(
+    "What does Au stand for in the periodic table? a. sodium, b. mercury, c. gold, d.oxygen"
+  );
+  if (userChoice === "c" || "c".toUpperCase()) {
+    alert("That is correct");
+    question2();
+  } else if (userChoice != "c" || "c".toUpperCase() || timer == -1) {
+    alert("That is incorrect");
+    question2();
+  }
+};
 
+const question2 = () => {
+  window.prompt(
+    "Which popular video game franchise has released games with the subtitled Modern Warfare and Black Ops? a. Call of Duty, b. Rainbow Six, c. Battlefield, d. Super Mario Bros"
+  );
+  if (userChoice === "a" || "a".toUpperCase()) {
+    alert("That is correct");
+  }
+};
+
+startTheGame();
