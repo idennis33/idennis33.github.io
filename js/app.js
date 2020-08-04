2da221632d07cdab97d9434a351c561bbda3d092
Isaac Dennis <idennisgsu3@gmail.com>
2020-08-03 21:07:47 -0400

    Got my the question to run in order and stop after they all have been asked. Also correct/wrong statemnt pops up

--- js/app.js
+++ js/app.js
@@ -8,20 +8,18 @@ const countDown = () => {
   if (timer == -1) {
     clearTimeout(timerId);
   } else {
-    elem.innerHTML = timer + " seconds remaining";
+    $("#timer").text(`${timer} seconds remaining`);
     timer--;
   }
 };
 
 let timerId = setInterval(countDown, 1000);
 
-let points = 0; 
-const scr = document.getElementById("scoreboard"); 
+let points = 0;
+const scr = document.getElementById("scoreboard");
 const addPoints = () => {
-  let score =  points++
-}
-
-
+  let score = points++;
+};
 
 $(() => {
   //Grabbing Elements
@@ -38,64 +36,70 @@ $(() => {
   //Event Listeners
   $openBtn.on("click", openModal);
   $closeBtn.on("click", closeModal);
-
-  
 });
 
 const startTheGame = () => {
   userChoice = prompt("Are you ready to be questioned?");
-  if (userChoice === "yes" || userChoice === "Yes" ) {
-    alert("Let's see what you know!!")
-  } else if (userChoice === "no" || userChoice === "No" ) {
+  if (userChoice === "yes" || userChoice === "Yes") {
+    alert("Let's see what you know!!");
+  } else if (userChoice === "no" || userChoice === "No") {
     alert("That's okay you need more time to study.");
-    endGame(); 
+    endGame();
   }
 };
 
-class Question{
-  constructor(question, answer,countDown){
-    this.question = question
-    this.answer= answer
+class Question {
+  constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
   }
 }
 const q1 = new Question(
-  'What does Au stand for in the periodic table? /n 1-sodium /n 2-mercury /n 3-gold /n 4-oxygen',
-  '3'
-)
+  "What does Au stand for in the periodic table? 1-sodium  2-mercury  3-gold  4-oxygen",
+  "3"
+);
 const q2 = new Question(
-  'Which popular video game franchise has released games with the subtitled Modern Warfare and Black Ops? /n 1-Call of Duty /n 2-Rainbow Six /n  3-Battlefield /n 4-Super Mario Bros',
-  '1'
-)
+  "Which popular video game franchise has released games with the subtitled Modern Warfare and Black Ops? 1-Call of Duty  2-Rainbow Six   3-Battlefield 4-Super Mario Bros",
+  "1"
+);
 const q3 = new Question(
-  'What is rapper P Diddy’s real name? /n 1-Chris Brown /n 2-Sean Adams /n 3-Micheal Jones /n 4-Sean Combs',
-  '4'
-)
+  "What is rapper P Diddy’s real name? 1-Chris Brown 2-Sean Adams  3-Mike Jones  4-Sean Combs",
+  "4"
+);
 const q4 = new Question(
-  'In which year was the Nintendo 64 released in Europe? /n 1-1995 /n 2-1996 /n 3-1997 /n 4-2000',
-  '3'
-)
+  "In which year was the Nintendo 64 released in Europe? 1-1995, 2-1996, 3-1997, 4-2000",
+  "3"
+);
 const q5 = new Question(
-  'Which European city hosted the 1936 Summer Olympics? /n 1-Berlin /n 2-Greece /n 3-Rome /n 4-Paris',
-  '1'
-)
-const questions = [q1, q2, q3,q4,q5]
-currentQuestion = undefined
+  "Which European city hosted the 1936 Summer Olympics?  1-Berlin  2-Greece 3-Rome  4-Paris",
+  "1"
+);
+const questions = [q1, q2, q3, q4, q5];
+let currentQuestion = undefined;
+const highestIndex = questions.length - 1;
+let currentQuestionIndex = 0; 
 // DOM targets
-const question = $('#question')
-$('#submit').on('click', (e)=>{
-  const answer = $('#answer').val()
-  console.log(answer)
-  if(answer===currentQuestion.answer){
-      $('#messsage').text('Correct answer!')
-  } else {
-      $('#messsage').text(`Wrong! the correct answer was ${currentQuestion.answer}`)
+const question = $("#question");
+$("#submit").on("click", (e) => {
+  const answer = $("#answer").val();
+  // console.log(answer)
+  if (answer === currentQuestion.answer) {
+    $("#message").text("Correct answer!");
+    addPoints();
+  } else if (answer !== currentQuestion.answer) { 
+    $("#message").text(`Wrong! the correct answer was  ${currentQuestion.answer}`);
   }
-})
-$('#new-question').on('click', (e)=>{
-  const idx = Math.floor(Math.random()*questions.length)
-  currentQuestion = questions[idx]
-  question.html(`Question: ${currentQuestion.question}`)
-})
+});
+$("#new-question").on("click", (e) => {
+    currentQuestion = questions[currentQuestionIndex];
+    question.html(`Question ${currentQuestion.question}`);
+    if (currentQuestionIndex < highestIndex) {
+      // if it's NOT the highest index, incriment the currentImgIndex by 1
+      currentQuestionIndex++;
+    }else {
+      endGame(); 
+    }
+});
 
 const endGame = () => {
   alert("Lets see your final score.");
