87903fe6d5b13d2f50b06f6b085da12fb7a5742a
Isaac Dennis <idennisgsu3@gmail.com>
2020-08-05 18:56:15 -0400

    Added readme.me file and a few changes to app.js

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # trivia-night
+/// 
+The different techonologies I used were originally tried to make a bunch of different functions that run the different levels. However ran into problems were it was all being ran in the console but not the DOM. So I reappoarched it where I decided to make a class called Question which had parameters of question and answer. From there I put the the instances of quesitons into a array called questions. I then made if statements for my on click functions for my submit and my new question button. So in the new button if a certian condition is met then the funcitons would invoke a endGame funciton. That would ask if the user(s) want to play again. If the user doesn't want to play again then I would remove the id of message and add a new text of Game Over. Telling the user the game is over. 
+////
+///// Unsolved Problems /////
+I had a problem where my code wouldn't actaully read the array postions correctly. Like poistion 0 wasn't the first question then the last question q5 wasn't the last index of the array. Another unsolved problem was that the timer ran as soon as I enter the webpage and I would have liked to have it start once I actually gotten through the startGame function. 
+
+//// Future Updates //// 
+I think adding some more questions, maybe actually having clickable choices, and a place where the players score could have been kept would be some nice future updates. 
+
+
+/// Link to live site /////
--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
 // console.log("hello");
 // console.log($);
-
+//// This is my timer that runs down from 75 seconds.
 let timer = 75;
 const elem = document.getElementById("timer");
-
-// const countDown = () => {
-//   if (timer == -1) {
-//     clearTimeout(timerId);
-//     endGame();
-//   } else {
-//     $("#timer").text(`${timer} seconds remaining`);
-//     timer--;
-//   }
-// };
-
-// let timerId = setInterval(countDown, 1000);
-
+const countDown = () => {
+  if (timer == -1) {
+    clearTimeout(timerId);
+    endGame();
+  } else {
+    $("#timer").text(`${timer} seconds remaining`);
+    timer--;
+  }
+};
+let timerId = setInterval(countDown, 1000);
+////  This adds my points to see the score after each session of the questions ran.
 let points = 0;
 const scr = document.getElementById("scoreboard");
 const addPoints = () => {
   let score = points++;
 };
-
 $(() => {
   //Grabbing Elements
   const $openBtn = $("#openModal");
@@ -38,27 +35,28 @@ $(() => {
   $openBtn.on("click", openModal);
   $closeBtn.on("click", closeModal);
 });
-
+// Start Game Functions that runs after user choice is yes. If userchoice is no an alert is invoked and endGame function is ran
 const startTheGame = () => {
   userChoice = prompt("Are you ready to be questioned?");
   if (userChoice === "yes" || userChoice === "Yes") {
     alert("Let's see what you know!!");
-    currentQuestion = questions[currentQuestionIndex]; 
+    currentQuestion = questions[currentQuestionIndex];
   $('#question').html(`Question:  ${currentQuestion.question}`);
-    currentQuestionIndex++; 
+    // currentQuestionIndex++;
     countDown();
   } else if (userChoice === "no" || userChoice === "No") {
     alert("That's okay you need more time to study.");
     endGame();
   }
 };
-
+//// Class of question which has two parameters of question and answer
 class Question {
   constructor(question, answer) {
     this.question = question;
     this.answer = answer;
   }
 }
+//// My instances of the Question class which has 5 quesitons.
 const q1 = new Question(
   "What does Au stand for in the periodic table? 1-sodium  2-mercury  3-gold  4-oxygen",
   "3"
@@ -79,10 +77,10 @@ const q5 = new Question(
   "Which European city hosted the 1936 Summer Olympics?  1-Berlin  2-Greece 3-Rome  4-Paris",
   "1"
 );
-
-const questions = [q1, q2, q3, q4, q5];
+/// I have an array of the quesitons instances called [questions]
+const questions = [q1, q2, q3, q4, q5,q5];
 let currentQuestion = undefined;
-let highestIndex = questions.length ;
+let highestIndex = questions.length-1;
 let currentQuestionIndex = 0;
 // DOM targets
 const question = $("#question");
@@ -93,16 +91,16 @@ $("#submit").on("click", (e) => {
     $("#message").text("Correct answer!");
     addPoints();
   } else if (answer !== currentQuestion.answer) {
-    $("#message").text(
-      `Wrong! the correct answer was  ${currentQuestion.answer}`
-    );
+    $("#message").text( `Wrong! the correct answer was  ${currentQuestion.answer}`);
+    
   }
 });
+// My two on-click functions for the new questions button and submit button.
 $("#new-question").on("click", (e) => {
-  $("#question").empty();
+  // $("#question").empty();
   console.log(currentQuestion)
-  currentQuestion = questions[currentQuestionIndex]; 
-  $('#question').html(`Question:  ${currentQuestion.question}`);
+  currentQuestion = questions[currentQuestionIndex];
+  $('#question').text(`Question: ${currentQuestion.question}`);
   console.log(highestIndex)
   console.log(currentQuestionIndex)
   if (currentQuestionIndex < highestIndex) {
@@ -110,22 +108,10 @@ $("#new-question").on("click", (e) => {
     console.log(currentQuestionIndex)
     currentQuestionIndex++;
     }else if(currentQuestionIndex === highestIndex){
-      endGame(); 
-    // if it's NOT the highest index, incriment the currentImgIndex by 1
-    
-  } 
-  
-  // const countDown = () => {
-  //   if (timer == -1) {
-  //     clearTimeout(timerId);
-  //     endGame();
-  //   } else {
-  //     $("#timer").text(`${timer} seconds remaining`);
-  //     timer--;
-  //   }
-  // };
+      endGame();
+  }
 });
-
+//// Endgame function that prompts user if they want to repeat the quesitons again if not GAME OVER!!!
 const endGame = () => {
   alert("Lets see your final score.");
   alert(`You got ${points} out of 5`);
@@ -140,7 +126,7 @@ const endGame = () => {
     return;
   }
 };
-
+ //// On click
 $("#startGame").on("click", (e) => {
   startTheGame();
 });
